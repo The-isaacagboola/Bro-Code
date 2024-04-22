835d67f42fc3fe7682e85466f7e35c190dbbf0d5
The _IsaacAgboola <isaacadeyemi9@gmail.com>
2024-04-22 12:43:16 +0100

    Bro Code Excercises

---
+++ my-react-practice/src/Practice1/ex1.jsx
@@ -0,0 +1,28 @@
+
+
+function Practice1 () {
+    return (
+        <div>
+            <h1 className="header">My Website</h1>
+            <ul className="nav">
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+                <li>Business</li>
+            </ul>
+
+            <h2>Select Your Prefered Meal Below</h2>
+
+            <div> Fruits
+                <ul>
+                    <li>Mango</li>
+                    <li>Banana</li>
+                    <li>Orange</li>
+                    <li>Lemon</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Practice1;
---
+++ my-react-practice/src/Practice1/footer.jsx
@@ -0,0 +1,8 @@
+
+
+function Footer(){
+    return (
+        <div> && Isaac Agboola, {(new Date).getFullYear()}</div>
+    )
+}
+export default Footer;
